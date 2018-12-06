81a4bc10249c62cefc4d42bf993e4d68068e7194
myaff <grabka74@gmail.com>
2018-12-07 02:11:25 +0500

    add marks

--- src/js/components/map.js
+++ src/js/components/map.js
@@ -6,24 +6,45 @@
 
 function init() {
   
-  let map = new ymaps.Map("map", {
-    // Координаты центра карты.
-    // Порядок по умолчанию: «широта, долгота».
-    // Чтобы не определять координаты центра карты вручную,
-    // воспользуйтесь инструментом Определение координат.
-    center: [55.80433406892266,37.585969499999955],
-    // Уровень масштабирования. Допустимые значения:
-    // от 0 (весь мир) до 19.
-    zoom: 17
-  });
-  let mark = new ymaps.Placemark([55.80433406892266,37.585969499999955], {}, {
-    iconLayout: 'default#image',
-    iconImageHref: '/build/img/marker.png',
-    iconImageSize: [60, 60],
-    iconImageOffset: [-25, -80]
-  });
-  map.geoObjects.add(mark);
-  map.behaviors.disable('scrollZoom');
+  if ($('#contact-map').length) {
+    let contactsMapId = "contact-map";
+    let contactsMapCoord = $(`#${contactsMapId}`).data('center').split(",");
+    let contactsMap = new ymaps.Map(contactsMapId, {
+      center: contactsMapCoord,
+      zoom: 17
+    });
+    contactsMap.behaviors.disable('scrollZoom');
+    if ($(`#${contactsMapId}`).data('placemark')) {
+      let contactsMapPlacemarkCoord = $(`#${contactsMapId}`).data('placemark').split(",");
+      let contactsMapPlacemark = new ymaps.Placemark(contactsMapPlacemarkCoord, {}, {
+        iconLayout: 'default#image',
+        iconImageHref: '/build/img/marker.png',
+        iconImageSize: [60, 60],
+        iconImageOffset: [-25, -80]
+      });
+      contactsMap.geoObjects.add(contactsMapPlacemark);
+    }
+  }
+
+  if ($('#about-map').length) {
+    let aboutMapId = "about-map";
+    let aboutMapCoord = $(`#${aboutMapId}`).data('center').split(",");
+    let aboutMap = new ymaps.Map(aboutMapId, {
+      center: aboutMapCoord,
+      zoom: 17
+    });
+    aboutMap.behaviors.disable('scrollZoom');
+    if ($(`#${aboutMapId}`).data('placemark')) {
+      let aboutMapPlacemarkCoord = $(`#${aboutMapId}`).data('placemark').split(",");
+      let aboutMapPlacemark = new ymaps.Placemark(aboutMapPlacemarkCoord, {}, {
+        iconLayout: 'default#image',
+        iconImageHref: '/build/img/marker-2.png',
+        iconImageSize: [28, 72],
+        iconImageOffset: [-14, -85]
+      });
+      aboutMap.geoObjects.add(aboutMapPlacemark);
+    }
+  }
 }
 
  module.exports = {init};
